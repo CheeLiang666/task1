d3004973ab4b8fd410b7407dda0b4343e655022c
CheeLiang666 <ccheeliang@gmail.com>
2019-02-19 17:17:41 +0800

    #4 Fixed: added a new page which display all kfc stores location.

--- mykfcsite/kfcstores/urls.py
+++ mykfcsite/kfcstores/urls.py
@@ -8,4 +8,7 @@ urlpatterns = [
     path('', views.listing, name='index'),
     # ex: /kfcstores/4715/
     path('<int:pk>/', views.StoresDetailView.as_view(), name='detail'),
+    # ex: /kfcstores/maps/
+    path('maps/', views.drawKFCMap, name='maps'),
+    path('maplocation/', views.getKFCsLatLng, name='maplocation'),
 ]
--- mykfcsite/kfcstores/views.py
+++ mykfcsite/kfcstores/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, JsonResponse
 from django.urls import reverse
 from django.views import generic
 from .models import Stores
@@ -12,6 +12,18 @@ def listing(request):
     page = request.GET.get('page', 1)
     stores = paginator.get_page(page)
     return render(request, 'kfcstores/index.html', {'stores': stores})
+
+def getKFCsLatLng(request):
+    stores_list = Stores.objects.all()
+    jsonLocationData = []
+    for store in stores_list:
+        locationDict = {'id': store.storeid, 'name': store.storename, 'address': store.storeaddress, 
+        'phone': store.storephone, 'latitude': store.latitude, 'longitude': store.longitude}
+        jsonLocationData.append(locationDict)
+    return JsonResponse({"data": jsonLocationData})
+
+def drawKFCMap(request):
+    return render(request, 'kfcstores/map.html')
 
 class StoresDetailView(generic.DetailView):
     model = Stores
